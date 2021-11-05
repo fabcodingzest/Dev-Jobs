71bf571f63ce7902b5ffa1dc18439e571b3880f9
fabcodingzest <fabrizvi786@gmail.com>
2021-11-05 16:12:36 +0530

    Adding font family to extended theme and testing the tweak i made for 1rem = 10 px for easy work

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-violet-dark">
-      Hello
+    <div className="bg-violet-dark font-KumbhSans">
+      <p className="text-[4rem] font-thin">Almost before we knew it</p>
     </div>
   );
 }
