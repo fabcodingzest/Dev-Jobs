2d5b1eeee060d084712c88f8707c069c36996c5e
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 01:35:23 +0530

    coding mode switch

--- src/components/ModeSwitch.js
+++ src/components/ModeSwitch.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { ReactComponent as MoonIcon } from '../assets/desktop/icon-moon.svg';
 import { ReactComponent as SunIcon } from '../assets/desktop/icon-sun.svg';
+import { useDarkMode } from '../hooks/toggleMode';
 
 const ModeSwitch = () => {
+  const [isDark, setIsDark] = useDarkMode();
+  console.log(isDark)
   return (
     <div className="flex justify-between items-center w-[5.6rem]">
       <SunIcon />
       <div>
-        <input type="checkbox" name="mode" id="toggle" className="hidden" />
+        <input checked={isDark} onChange={(e) => setIsDark(e.target.checked)} type="checkbox" name="mode" id="toggle" className="hidden" />
         <label htmlFor="toggle">
           <div className="cursor-pointer w-[2.8rem] h-[1.4rem] flex items-center bg-white rounded-full p-[0.4rem]">
             <div className="toggle-dot transform duration-300 ease-in-out w-[0.8rem] h-[0.8rem] bg-violet-dark rounded-full"></div>
