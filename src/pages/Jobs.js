148d6cbc34881fa766aadba25f10522116ca61e0
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 20:27:49 +0530

    using imported api function

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,31 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import getData from '../api/api';
+import Button from '../components/Button';
 import Container from '../components/Container';
 import JobCard from '../components/JobCard';
 
 const Jobs = () => {
   const [data, setData] = useState([]);
-  const getData = async () => {
-    const res = await fetch('data.json', {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    });
-    const data = await res.json();
+  const setState = async () => {
+    const data = await getData();
     setData(data);
   };
   useEffect(() => {
     setTimeout(() => {
-      getData();
+      setState();
     }, 1000);
   }, []);
   if (!data || data.length === 0) return <p>loading...</p>;
   return (
-    <Container>
-      <div className="grid grid-cols-3 gap-x-6 gap-y-12 -mt-8 relative z-10">
+    <Container className="pb-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12 -mt-8 relative z-10 pb-10">
         {data.map((item) => (
-          <JobCard key={item.id} detail={item} />
+          <Link to={`../job-detail/${item.id}`} key={item.id} className="flex justify-center w-full">
+            <JobCard detail={item} />
+          </Link>
         ))}
+      </div>
+      <div className="flex justify-center pb-10">
+        <Button text="Load More" className="bg-violet-dark text-white hover:bg-violet-light" />
+        {/* <Button text="Load More" className="bg-violet-dark bg-opacity-10 text-violet-dark hover:bg-violet-dark hover:bg-opacity-40" />
+        <Button text="Load More" className="bg-grey-btn text-white hover:bg-grey-hover" /> */}
       </div>
     </Container>
   );
