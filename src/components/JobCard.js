73146a12f786af1351d3123d6d4c418acf4036f5
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 20:27:21 +0530

    Completing job cards component

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const JobCard = ({ detail }) => {
   return (
-    <div className="bg-white dark:bg-blue-dark p-[1.6rem] rounded-md relative">
+    <div className="bg-white dark:bg-blue-dark p-[1.6rem] rounded-md relative w-full max-w-[17rem]">
       <div className="w-[2.5rem] h-[2.5rem] bg-grey-dark rounded-md flex justify-center items-center absolute -top-4 left-[1.6rem]" style={{ backgroundColor: detail.logoBackground }}>
         <img src={process.env.PUBLIC_URL + detail.logo} alt={detail.company} />
       </div>
@@ -14,7 +14,7 @@ const JobCard = ({ detail }) => {
         </p>
         <p className="font-bold py-2">{detail.position}</p>
         <p className="text-grey-dark text-xs">{detail.company}</p>
-      <p className="text-xs text-violet-dark font-bold pt-8">{detail.location}</p>
+        <p className="text-xs text-violet-dark font-bold pt-8">{detail.location}</p>
       </div>
     </div>
   );
