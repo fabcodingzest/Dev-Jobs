0ce2bffc879ad9b80cc3864d75de662593de1c83
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 20:52:46 +0530

    fetching Job detail data from api and its working

--- src/pages/JobDetail.js
+++ src/pages/JobDetail.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import getData from '../api/api';
+import { useParams } from 'react-router-dom';
 
 const JobDetail = () => {
-  return <div className="bg-green-200 text-green-900">Details of the job</div>;
+  const { id } = useParams();
+  const [details, setDetails] = useState();
+  const setState = async () => {
+    const data = await getData();
+    const [jobDetails] = data.filter((job) => job.id.toString() === id);
+    setDetails(jobDetails);
+  };
+  useEffect(() => {
+    setState();
+  });
+  if (!details || details.length === 0) return <p>loading...</p>;
+  return (
+    <div className="bg-green-200 text-green-900">
+      Details of the job- {id}
+      <h3>{details.company}</h3>
+    </div>
+  );
 };
 
 export default JobDetail;
