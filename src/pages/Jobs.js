ccd336ddfccf29df972be4d6954a0150517e0a9a
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 16:36:14 +0530

    Adding Jobs page

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Container from '../components/Container';
+import JobCard from '../components/JobCard';
 
 const Jobs = () => {
-  return <div className="bg-red-300 text-red-900">Jobs Page</div>;
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const res = await fetch('data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+    const data = await res.json();
+    setData(data);
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      getData();
+    }, 1000);
+  }, []);
+  if (!data || data.length === 0) return <p>loading...</p>;
+  return (
+    <Container>
+      <div className="grid grid-cols-3 gap-x-6 gap-y-12 -mt-8 relative z-10">
+        {data.map((item) => (
+          <JobCard key={item.id} detail={item} />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Jobs;
