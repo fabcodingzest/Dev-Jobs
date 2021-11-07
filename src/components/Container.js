de246e781804b3091771e2a50c40f3639b5b3686
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 23:36:19 +0530

    changes in container for responsiveness

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Container = ({ children, className }) => {
-  return <div className={`${className} px-[0.7rem] md:mx-auto lg:max-w-[56.9rem]`}>{children}</div>;
+  return <div className={`${className} px-[0.7rem] sm:px-10 md:mx-auto lg:max-w-[56.9rem]`}>{children}</div>;
 };
 
 export default Container;
