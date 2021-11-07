1eae404e23b3ed33bfb51efd15802a1c2b029471
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 23:36:05 +0530

    button to use  icon

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Button = ({ text, className }) => {
-  return <button className={`mx-2 px-6 py-2 rounded ${className} font-semibold`}>{text}</button>;
+const Button = ({ text, className, icon }) => {
+  return (
+    <button className={`px-1.5 lg:px-4 xl:px-6 py-2 rounded ${className} font-semibold`}>
+      {icon}
+      {text}
+    </button>
+  );
 };
 
 export default Button;
