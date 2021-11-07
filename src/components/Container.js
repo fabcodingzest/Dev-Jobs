c7d676046953f66127cad789a0aca60a1b695432
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 16:35:21 +0530

    Adding container component

---
+++ src/components/Container.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Container = ({ children }) => {
+  return <div className="px-[0.7rem] md:mx-auto md:max-w-[35.5rem] lg:max-w-[56.9rem]">{children}</div>;
+};
+
+export default Container;
