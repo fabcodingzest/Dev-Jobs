3290e0cdba2b39289703f69db57e465ae7e315cb
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 16:35:37 +0530

    Adding Container to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../assets/desktop/logo.svg';
+import Container from './Container';
 import ModeSwitch from './ModeSwitch';
 
 const Header = () => {
   return (
     <div className="bg-violet-dark bg-header-desktop h-[6.8rem] md:h-[8.1rem] rounded-bl-[5rem]">
-      <div className="px-[0.7rem] md:mx-auto md:max-w-[35.5rem] lg:max-w-[56.9rem]">
+      <Container>
         <div className="pt-[1.6rem] md:pt-[2.1rem] flex justify-between">
           <Logo />
           <ModeSwitch />
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
