d947548660328f357e1a4f02ff98b0f74b2a19d1
fabcodingzest <fabrizvi786@gmail.com>
2021-11-07 23:36:30 +0530

    adding link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../assets/desktop/logo.svg';
 import Container from './Container';
 import ModeSwitch from './ModeSwitch';
 
 const Header = () => {
   return (
-    <div className="bg-violet-dark bg-header-desktop h-[6.8rem] md:h-[8.1rem] rounded-bl-[5rem]">
+    <div className="bg-violet-dark bg-header-desktop h-[6.8rem] md:h-[8.1rem] md:rounded-bl-[5rem]">
       <Container>
         <div className="pt-[1.6rem] md:pt-[2.1rem] flex justify-between">
-          <Logo />
+          <Link to="../">
+            <Logo />
+          </Link>
           <ModeSwitch />
         </div>
       </Container>
