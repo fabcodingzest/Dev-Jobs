e11bbbe93a7f41f4d0acaa5fe8e394df74d3438c
fabcodingzest <fabrizvi786@gmail.com>
2021-11-08 23:13:19 +0530

    updating btn and adding type

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = ({ text, className, icon }) => {
+const Button = ({ text, className, icon, type }) => {
   return (
-    <button className={`px-1.5 lg:px-4 xl:px-6 py-2 rounded ${className} font-semibold`}>
+    <button type={type} className={`px-1.5 lg:px-4 xl:px-[1.784rem] py-2 lg:py-[0.6rem] rounded ${className} font-semibold`}>
       {icon}
       {text}
     </button>
