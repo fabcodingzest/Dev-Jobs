76cad9adffcca822782ac2b7ffdffe90208135bd
fabcodingzest <fabrizvi786@gmail.com>
2021-11-08 23:13:35 +0530

    adding loading state and data handling

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -8,8 +8,10 @@ import Search from '../components/Search';
 
 const Jobs = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const setState = async () => {
     const data = await getData();
+    setLoading(false);
     setData(data);
   };
   useEffect(() => {
@@ -17,22 +19,29 @@ const Jobs = () => {
       setState();
     }, 1000);
   }, []);
-  if (!data || data.length === 0) return <p>loading...</p>;
   return (
     <Container className="pb-10 -mt-8">
       <Search setData={setData} />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12 relative z-10 py-10">
-        {data.map((item) => (
-          <Link to={`../job-detail/${item.id}`} key={item.id} className="flex justify-center w-full">
-            <JobCard detail={item} />
-          </Link>
-        ))}
-      </div>
-      <div className="flex justify-center pb-10">
-        <Button text="Load More" className="bg-violet-dark text-white hover:bg-violet-light" />
-        {/* <Button text="Load More" className="bg-violet-dark bg-opacity-10 text-violet-dark hover:bg-violet-dark hover:bg-opacity-40" />
-        <Button text="Load More" className="bg-grey-btn text-white hover:bg-grey-hover" /> */}
-      </div>
+      {loading ? (
+        <p>loading...</p>
+      ) : data?.length === 0 ? (
+        <p>No Jobs found...</p>
+      ) : (
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12 relative z-10 py-10">
+            {data.map((item) => (
+              <Link to={`../job-detail/${item.id}`} key={item.id} className="flex justify-center w-full">
+                <JobCard detail={item} />
+              </Link>
+            ))}
+          </div>
+          <div className="flex justify-center pb-10">
+            <Button text="Load More" className="bg-violet-dark text-white hover:bg-violet-light" />
+            {/* <Button text="Load More" className="bg-violet-dark bg-opacity-10 text-violet-dark hover:bg-violet-dark hover:bg-opacity-40" />
+          <Button text="Load More" className="bg-grey-btn text-white hover:bg-grey-hover" /> */}
+          </div>
+        </>
+      )}
     </Container>
   );
 };
