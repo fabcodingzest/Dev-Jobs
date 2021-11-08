0aa5976bd63095e256bab03c88e1154ee3c0425b
fabcodingzest <fabrizvi786@gmail.com>
2021-11-09 02:48:26 +0530

    changing hook to use useEffect hook

--- src/hooks/useSafeLocalStorage.js
+++ src/hooks/useSafeLocalStorage.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export function useSafeLocalStorage(key, initialValue) {
   const [valueProxy, setValueProxy] = useState(() => {
@@ -11,17 +11,19 @@ export function useSafeLocalStorage(key, initialValue) {
       return initialValue;
     }
   });
-
-  const setValue = (value) => {
-    try {
-      // set theme in the case of manual switch
-      window.localStorage.setItem(key, value);
-      setValueProxy(value);
-    } catch {
-      // just change the value if there was already stored value
-      setValueProxy(value);
-    }
-  };
-
-  return [valueProxy, setValue];
+  // No need for this function
+  // const setValue = (value) => {
+  //   try {
+  //     // set theme in the case of manual switch
+  //     window.localStorage.setItem(key, value);
+  //     setValueProxy(value);
+  //   } catch {
+  //     // just change the value if there was already stored value
+  //     setValueProxy(value);
+  //   }
+  // };
+      useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(valueProxy));
+      }, [key, valueProxy]);
+  return [valueProxy, setValueProxy];
 }
