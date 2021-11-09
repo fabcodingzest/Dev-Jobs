bebe0abddb9afa15f4df1113c6d5ee6051a05fe3
fabcodingzest <fabrizvi786@gmail.com>
2021-11-09 21:27:29 +0530

    changed btn component styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ text, className, icon, type }) => {
   return (
-    <button type={type} className={`px-1.5 lg:px-4 xl:px-[1.784rem] py-2 lg:py-[0.6rem] rounded ${className} font-semibold`}>
+    <button type={type} className={`px-2.5 lg:px-4 xl:px-[1.784rem] py-2 lg:py-[0.6rem] rounded ${className} font-semibold`}>
       {icon}
       {text}
     </button>
