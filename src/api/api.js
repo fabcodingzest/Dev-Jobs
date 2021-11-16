4b633da8504fbc8c03b38e5f18c029443fd30528
fabcodingzest <fabrizvi786@gmail.com>
2021-11-17 00:44:44 +0530

    making api work for specific data

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,12 @@
-const getData = async () => {
-  const res = await fetch('https://v1.nocodeapi.com/fab/google_sheets/ujOmDEDtkhVDvVGS?tabId=Sheet1', {
+const getData = async (id) => {
+  const res = await fetch(`https://v1.nocodeapi.com/fab/google_sheets/ujOmDEDtkhVDvVGS?tabId=Sheet1&${id ? `row_id=${id}` : ''}`, {
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
     },
   });
   const data = await res.json();
+  if (id) return data;
   return data.data;
 };
 export default getData;
